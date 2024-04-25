419258efd8c55adf7c262874ce10aaaa214dcd03
Qexat <43090614+qexat@users.noreply.github.com>
2024-04-25 18:01:19 +0200

    tests: add util + tests for it

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,3 +1,7 @@
+import functools
+import operator
+
+
 def double(x):
     return x * 2
 
@@ -8,3 +12,7 @@ def greater_than_four(x):
 
 def contains_letter_l(x):
     return "l" in x
+
+
+def reduce_right_sub(l):
+    return functools.reduce(operator.sub, reversed(l), 0)
